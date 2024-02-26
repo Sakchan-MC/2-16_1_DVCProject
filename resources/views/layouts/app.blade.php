f50e887288f358f6aac519a6360b1b738786a711
u6530852 <u6530852@serv.bithost.pongsawadi.ac.th>
2024-02-26 21:02:37 +0700

    edit script

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,50 +1,53 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+   <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet"/>
+   <!-- Fonts -->
+   <link rel="preconnect" href="https://fonts.bunny.net">
+   <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-    <!-- Scripts -->
-    <!-- @vite(['resources/css/app.css', 'resources/js/app.js']) -->
-    <link rel="stylesheet" href="{{ asset('build/assets/app-445b7fe1.css') }}">
-    <link rel="script" href="{{ asset('build/assets/app-d62ae1e6.js') }}">
-    @stack("script")
+   <!-- Scripts -->
+   <!-- @vite(['resources/css/app.css', 'resources/js/app.js']) -->
+   <link rel="stylesheet" href="{{ asset('build/assets/app-445b7fe1.css') }}">
+   <script src="{{ asset('build/assets/app-d62ae1e6.js') }}"></script>
+   @stack('script')
 
-    <!-- Styles -->
-    @livewireStyles
+   <!-- Styles -->
+   @livewireStyles
 </head>
+
 <body class="font-sans antialiased">
-<x-banner/>
+   <x-banner />
 
-<div class="min-h-screen bg-gray-100">
-    @livewire('navigation-menu')
+   <div class="min-h-screen bg-gray-100">
+      @livewire('navigation-menu')
 
-    <!-- Page Heading -->
-    @if (isset($header))
-        <header class="bg-white shadow">
-            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                {{ $header }}
+      <!-- Page Heading -->
+      @if (isset($header))
+         <header class="bg-white shadow">
+            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+               {{ $header }}
             </div>
-        </header>
-    @endif
+         </header>
+      @endif
 
-    <!-- Page Content -->
-    <main>
-        <div class="container mx-auto px-4">
+      <!-- Page Content -->
+      <main>
+         <div class="container mx-auto px-4">
             {{ $slot }}
-        </div>
-    </main>
-</div>
+         </div>
+      </main>
+   </div>
 
-@stack('modals')
+   @stack('modals')
 
-@livewireScripts
+   @livewireScripts
 </body>
+
 </html>
--- resources/views/layouts/welcome.blade.php
+++ resources/views/layouts/welcome.blade.php
@@ -1,50 +1,52 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+   <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet"/>
+   <!-- Fonts -->
+   <link rel="preconnect" href="https://fonts.bunny.net">
+   <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-    <!-- Scripts -->
-    <!-- @vite(['resources/css/app.css', 'resources/js/app.js']) -->
-    <link rel="stylesheet" href="{{ asset('build/assets/app-445b7fe1.css') }}">
-    <link rel="script" href="{{ asset('build/assets/app-d62ae1e6.js') }}">
-    @stack("script")
+   <!-- Scripts -->
+   <!-- @vite(['resources/css/app.css', 'resources/js/app.js']) -->
+   <link rel="stylesheet" href="{{ asset('build/assets/app-445b7fe1.css') }}">
+   <script src="{{ asset('build/assets/app-d62ae1e6.js') }}"></script>
+   @stack('script')
 
-    <!-- Styles -->
-    @livewireStyles
+   <!-- Styles -->
+   @livewireStyles
 </head>
+
 <body class="font-sans antialiased">
 
-<div class="bg-gray-300 h-max min-h-screen">
-    <div class="navbar">
-        <div class="flex-1">
-            <a class="btn btn-ghost normal-case text-xl" href="/">Rimna</a>
-        </div>
-        <div class="flex-none">
-            <ul class="menu menu-horizontal px-1 z-10">
-                <li><a href="{{route("product")}}" class="{{request()->routeIs('product')?'active':''}}">Products</a>
-                </li>
-                @if(Auth::user())
-                    <li><a href="{{route("admin.product")}}">Manage product</a></li>
-
-                @else
-                    <li><a href="{{route("login")}}">Login</a></li>
-                @endif
+   <div class="h-max min-h-screen bg-gray-300">
+      <div class="navbar">
+         <div class="flex-1">
+            <a class="btn btn-ghost text-xl normal-case" href="/">Rimna</a>
+         </div>
+         <div class="flex-none">
+            <ul class="menu menu-horizontal z-10 px-1">
+               <li><a href="{{ route('product') }}" class="{{ request()->routeIs('product') ? 'active' : '' }}">Products</a>
+               </li>
+               @if (Auth::user())
+                  <li><a href="{{ route('admin.product') }}">Manage product</a></li>
+               @else
+                  <li><a href="{{ route('login') }}">Login</a></li>
+               @endif
             </ul>
-        </div>
-    </div>
-    <div class="container mx-auto px-4 pb-4">
-        {{ $slot }}
-    </div>
-</div>
+         </div>
+      </div>
+      <div class="container mx-auto px-4 pb-4">
+         {{ $slot }}
+      </div>
+   </div>
 
-@livewireScripts
+   @livewireScripts
 </body>
+
 </html>
